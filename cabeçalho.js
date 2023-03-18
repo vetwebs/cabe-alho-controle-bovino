45926390d72f8b7d01c3ed002c258c1bdd8ecc32
Ana Rita Queiroz <anaritadequeiroz@hotmail.com>
2023-03-17 23:02:41 -0300

    atualização do mobile

--- cabeçalho.js
+++ cabeçalho.js
@@ -1,34 +1,11 @@
-class MobileNavbar {
-    constructor(mobileMenu, navList, navLinks) {
-      this.mobileMenu = document.querySelector(mobileMenu);
-      this.navList = document.querySelector(navList);
-      this.navLinks = document.querySelectorAll(navLinks);
-      this.activeClass = "active";
-  
-      this.handleClick = this.handleClick.bind(this);
-    }
-  
-    handleClick() {
-      this.navList.classList.toggle(this.activeClass);
-      this.mobileMenu.classList.toggle(this.activeClass);
-      this.animateLinks();
-    }
-  
-    addClickEvent() {
-      this.mobileMenu.addEventListener("click", this.handleClick);
-    }
-  
-    init() {
-      if (this.mobileMenu) {
-        this.addClickEvent();
-      }
-      return this;
-    }
-  }
-  
-  const mobileNavbar = new MobileNavbar(
-    ".mobile-menu",
-    ".nav-list",
-    ".nav-list li",
-  );
-  mobileNavbar.init();
+
+const menuDiv = document.getElementById('navegação-menu-mobile')
+
+const btnAnimar = document.getElementById('btn-menu')
+
+menuDiv.addEventListener('click', animar)
+
+function animar(){ 
+    menuDiv.classList.toggle('abrir')
+    btnAnimar.classList.toggle('ativar')
+}
